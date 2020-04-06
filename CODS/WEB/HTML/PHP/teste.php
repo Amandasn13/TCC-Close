ebeb1da5e466b14b29a670ba89da9d96b3c4f0a9
Ditooxsz <dudubtr75@gmail.com>
2020-04-06 17:30:47 -0300

    Create teste.php

---
+++ CODS/WEB/HTML/PHP/teste.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['IdUsuario']))
+    {
+        header("location: ../Close_Log.php");
+        exit;
+    }
+
+?>
+
+
+
+SEJA BEMMMMMM VINDOOOOOOO
