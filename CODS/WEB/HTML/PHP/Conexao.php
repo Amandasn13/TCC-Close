9ae0a89f7479c96d17d59cdb8be2a70b7890295a
Ditooxsz <dudubtr75@gmail.com>
2020-07-15 08:57:27 -0300

    Update Conexao.php

--- CODS/WEB/HTML/PHP/Conexao.php
+++ CODS/WEB/HTML/PHP/Conexao.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $passowrd = "";
-$db_name = "login";
+$db_name = "Cloose";
 
 $connect = mysqli_connect($servername, $username, $passowrd, $db_name);
 ?>
