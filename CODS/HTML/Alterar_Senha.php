5aaf24bf89ced179987b625e94add3e99399834c
Amandasn13 <61799153+Amandasn13@users.noreply.github.com>
2020-11-25 22:39:55 -0300

    Atualização 25112020_Esqueci e Alterar senha (design)

--- CODS/HTML/Alterar_Senha.php
+++ CODS/HTML/Alterar_Senha.php
@@ -14,26 +14,60 @@ $dados1 = mysqli_fetch_array($resultado);
 ?>
 
 <!DOCTYPE html>
-<html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Esqueci Minha Senha</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuw
+    vrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF
+    5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <link rel="icon" type="imagem/png" href="IMG\Vetores\Close_Logo.png">
 </head>
-<body bgcolor = "#DC143C">
+<style>
+    body, html{
+        height:100%;
+        }
+    .bg{
+        background-image: linear-gradient(to left top, #ffbd00, #ffa20a, #ff871e, #ff6a2f, 
+        #ff4b3e, #ff365c, #ff297b, #f72b99, #dc51c6, #b270e8, #7887fb, #0099ff); 
+        background-repeat: no-repeat;
+        background-size: cover; 
+        height: 100%; 
+        background-position: center;
+    }
+</style>
+<body class="bg">
+<br><br><br><br><br><br>
     <?php
         $email = $dados1['email'];
         $sql = "SELECT IdUsuario FROM Usuario WHERE E_mail = '$email'";
         $resultado1 = mysqli_query($connect, $sql);
         $dados2 = mysqli_fetch_array($resultado1); 
     ?>
-    <center><H1>Digite sua nova senha e a confirmação!</H1>
-    <form method="post">
-    <input type="hidden" value="<?php echo $rash; ?>" name="rash">   
-    <input type="hidden" value="<?php echo $dados2['IdUsuario'];?>" name="iduser">
-    <input type = "password" name ="novasenha" placeholder="Digite a nova senha aqui!" required><br><br>
-    <input type = "password" name ="confirmanovasenha" placeholder="Digite a confirmação de senha!" required><br><br>
-    <input type = "submit" name ="Confirmar" Value = "Alterar senha"></center>
-    <?php
+    <center>
+    <div class="jumbotron" style="width: 60%; background-color: #141414;">
+        <H1 style="color: azure;">Digite sua nova senha e a confirmação!</H1><br>
+        <form method="post">
+            <input type="hidden" value="<?php echo $rash; ?>" name="rash">   
+            <input type="hidden" value="<?php echo $dados2['IdUsuario'];?>" name="iduser">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text bg-warning" id="inputGroup-sizing-default" style="height: 38px;">Senha</span>
+                </div>
+                <input type = "password" class="form-control" name ="novasenha" 
+                placeholder="Digite a nova senha aqui!" required><br><br>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text bg-warning" id="inputGroup-sizing-default" style="height: 38px;">Confirmação</span>
+                </div>
+                <input type = "password" class="form-control" name ="confirmanovasenha" 
+            placeholder="Digite a confirmação de senha!" required><br><br>
+            </div>
+            <input type = "submit" class="btn btn-primary" name ="Confirmar" Value = "Alterar senha"><br>
+            <?php
 
                                     if(isset($_POST['novasenha']))
                                     {
@@ -70,7 +104,8 @@ $dados1 = mysqli_fetch_array($resultado);
                                     </script>";
                                 }
                             }
-                                ?>
-    </form>          
+            ?>
+        </form>  
+    </div></center>        
 </body>
 </html>
--- CODS/HTML/Esqueci_Senha.php
+++ CODS/HTML/Esqueci_Senha.php
@@ -1,39 +1,70 @@
-
 <!DOCTYPE html>
-<html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Esqueci Minha Senha</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuw
+    vrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF
+    5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <link rel="icon" type="imagem/png" href="IMG\Vetores\Close_Logo.png">
 </head>
-<body bgcolor = "#DC143C">
-    <center><H1>Esqueceu a Senha?</H1>
-    <H3>Basta Informar seu E-mail abaixo, para que possa alterar sua senha!</H3>
-    <form method="post">
-    <input type = "text" name ="Email"><br><br>
-    <input type = "submit" name ="Enviar" Value = "Enviar"></center>
-    <?php
-    require_once 'PHP/Login_Cadastro.php';
+<style>
+    body, html{
+        height:100%;
+        }
+    .bg{
+        background-image: linear-gradient(to right bottom, #ffbd00, #ffa20a, #ff871e, #ff6a2f, 
+        #ff4b3e, #ff365c, #ff297b, #f72b99, #dc51c6, #b270e8, #7887fb, #0099ff); 
+        background-repeat: no-repeat;
+        background-size: cover; 
+        height: 100%; 
+        background-position: center;
+    }
+</style>
+<body class="bg">
+<br><br><br><br><br><br>
+<section style="heigh; 100%;">
+<center>
+<div class="jumbotron" style="width: 60%; background-color: #141414;">
+    <h1 class="display-4" style="color: azure;">Esqueceu a Senha?</h1>
+    <p class="lead" style="color: azure;">Basta Informar seu E-mail abaixo, para que possa alterar sua senha!</p>
+        <div class="input-group mb-3">
+            <input type="text" name ="Email" class="form-control" placeholder="Digite seu email aqui" 
+            aria-label="Recipient's username" aria-describedby="button-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-success" name="Enviar" type="button" id="button-addon2"
+                style="height: 38px; border-top-right-radius: 5px; border-bottom-right-radius: 5px;">Enviar</button>
+                <?php
+                    require_once 'PHP/Login_Cadastro.php';
 
-    $u = new Usuario; 
+                    $u = new Usuario; 
 
-                                    if(isset($_POST['Email']))
-                                    {
-                                        $email = addslashes($_POST['Email']);
-                                                    
+                    if(isset($_POST['Email']))
+                    {
+                        $email = addslashes($_POST['Email']);
+                                                                
+                                                            
+
+                        $u->conexao("Tiffanny", "localhost","root","");
+                        if($u->msgErro == "")
+                        {
+                                                        
+                            $u->esquecisenha($email);
                                                 
+                        }else
+                        {
+                            echo "Erro: ".$u->msgErro;
+                        }
+                    }
+                ?>
+            </div><br><br>
+        </div>
+</div>
+</center>
 
-                                        $u->conexao("Tiffanny", "localhost","root","");
-                                        if($u->msgErro == "")
-                                        {
-                                            
-                                            $u->esquecisenha($email);
-                                    
-                                        }else
-                                        {
-                                            echo "Erro: ".$u->msgErro;
-                                        }
-                                    }
-                                ?>
-                      </form>          
+
+</section>     
 </body>
 </html>
