fb0d9da2286628e8219900392fb1923d98d31f3b
Lux <62267570+Gui-art@users.noreply.github.com>
2020-11-29 14:38:16 -0300

    Novo arquivo para funções das ropuas!

--- CODS/HTML/PHP/Login_Cadastro.php
+++ CODS/HTML/PHP/Login_Cadastro.php
@@ -141,52 +141,6 @@ public function editar1($id, $nome, $sobrenome, $nascimento)
 
     }
 
-
-//ediçaõ do titulo/nome da roupa!
-public function editartitulo($id, $titulo, $categoria, $tipo, $cor, $descricao, $tamanho, $marca, $material)
-{
-    
-    global $pdo;
-    global $msgErro;
-   
-        
-        $sql = $pdo->prepare("CALL Alterar_Roupa(:id, :tit, :cat, :tip, :c, :descr, :tam, :mar, :mat)");
-        $sql->bindValue(":id", $id);
-        $sql->bindValue(":tit", $titulo); 
-        $sql->bindValue(":cat", $categoria); 
-        $sql->bindValue(":tip", $tipo); 
-        $sql->bindValue(":c", $cor); 
-        $sql->bindValue(":descr", $descricao);  
-        $sql->bindValue(":tam", $tamanho); 
-        $sql->bindValue(":mar", $marca);
-        $sql->bindValue(":mat", $material);
-        $sql->nextRowset(); 
-        $sql->execute();
-        if($sql->rowCount() > 0)
-        {
-            return true;
-        }else{
-            return false;
-        }
-        
-    
-
-}
-
-public function apagarfoto($id)
-{
-    
-    global $pdo;
-    global $msgErro;
-   
-        $sql = $pdo->prepare("CALL Apagar_Roupa(:id)");
-        $sql->bindValue(":id", $id);
-        $sql->nextRowset();
-        $sql->execute();
-        return true;
-    
-
-}
 public function esquecisenha($email){
     
     global $pdo;
