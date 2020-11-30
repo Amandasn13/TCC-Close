19a98999efbda164a51eb4d11e643d0a3976b401
Amandasn13 <61799153+Amandasn13@users.noreply.github.com>
2020-11-30 16:35:38 -0300

    Atualização Desapegos - 30112020

--- CODS/HTML/Close_Desp.php
+++ CODS/HTML/Close_Desp.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'PHP/Conexao.php';
-require_once 'PHP/funcoes_usuario.php';
+require_once 'PHP/Login_Cadastro.php';
 session_start();
 if(!isset($_SESSION['IdUsuario']))
 {
@@ -48,12 +48,10 @@ $u = new Usuario;
                 document.getElementById("tt").style.color= "azure";
                 document.getElementById("t1").style.color= "azure";
                 document.getElementById("t2").style.color= "azure";
-                document.getElementById("t3").style.color= "azure";
                 document.getElementById("t4").style.color= "azure";
                 document.getElementById("t5").style.color= "azure";
                 document.getElementById("b1").style.color= "azure";
                 document.getElementById("b2").style.color= "azure";
-                document.getElementById("b3").style.color= "azure";
                 document.getElementById("b4").style.color= "azure";
                 document.getElementById("b5").style.color= "azure";
             }
@@ -61,12 +59,10 @@ $u = new Usuario;
                 document.getElementById("tt").style.color= "#070B19";
                 document.getElementById("t1").style.color= "#070B19";
                 document.getElementById("t2").style.color= "#070B19";
-                document.getElementById("t3").style.color= "#070B19";
                 document.getElementById("t4").style.color= "#070B19";
                 document.getElementById("t5").style.color= "#070B19";
                 document.getElementById("b1").style.color= "#151515";
                 document.getElementById("b2").style.color= "#151515";
-                document.getElementById("b3").style.color= "#151515";
                 document.getElementById("b4").style.color= "#151515";
                 document.getElementById("b5").style.color= "#151515";
             }
@@ -106,27 +102,24 @@ Nós da equipe Close separamos algumas informações sobre instituições que po
 <hr style="height: 2px; background-color: rgb(201, 198, 198); width: 90%; opacity: 30%;">
 <br><br>
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-around">
         <div class="col-lg-4">
             <img class="rounded-circle" src="IMG/Icones/gv.png" alt="imagem ilustrativa com uma bandeira brasileira 
             minimalista e os dizeres 'Governo do Brasil' para representar os diversos governos nacionais" width="200"
              height="200" style="border: 5px solid #393841;">
             <h2 id="t1">Iniciativas Governamentais</h2>
-            <p id="b1">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <p id="b1" style="text-align: justify;">Como exemplos de ações de instituições Governamentais relacionadas ao desapegos de roupas temos a Campanha do Agasalho que incentiva a doação de roupas, agasalhos e cobertores por parte da população e empresas durante os meses de
+             outono e inverno, quando a temperatura é menor. O exército quem funciona como uma central de triagem das roupas, onde grupo de voluntários recebe as peças e as separa entre homens, mulheres e crianças. Todas as roupas são lavadas e passadas e os voluntários também fazem 
+             a montagem de kits para as famílias que serão beneficiadas. Os pontos de coletas variam em cada estado a maior parte se encontram em escolas, postos de gasolina e de saúde e em alguns supermercados. Existem também estados que disponibilizam uma troca beneficiente direta.</p>
         </div>
         <div class="col-lg-4">
-            <img class="rounded-circle" src="IMG/Icones/rlg.png" alt="icone minimalista com simbolos que representam 
-            diversos tipos fé diferentes para representar religião" width="200" height="200" 
-            style="border: 5px solid #393841;">
-            <h2 id="t2">Instituições Religiosas</h2>
-            <p id="b2">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <img class="rounded-circle" src="IMG/Icones/mny.png" alt="icone ilustrativo de uma mão segurando um saco de dinheiro 
+            para representar as trocas comerciais" width="200" height="200" style="border: 5px solid #393841;">
+            <h2 id="t5">Trocas <br> Comerciais</h2>
+            <p id="b5" style="text-align: justify;">O quesito "Trocas Comerciais" diz respeito às relações de compra e venda - que se estendem desde domínios físicos até os virtuais. Além de lojas de grife, o terrítório nacional também apresenta um número crescente de brechós: somente entre os anos de 2010  e 2015, essas lojas - que compram roupas usadas para revender por preços geralmente menores e/ou mais acessíveis do que seus originais - tiveram um crescimento de 22,2%. 
+            No meio virtual existem dois tipos de comércios voltados à moda: os que só permitem que o usuário compre roupas, e os que permitem que o usuário não só compre como também venda peças próprias - esses sites geralmente pedem uma taxa porcentual do valor da venda para sua manuntenção ou uma taxa mensal; o parâmetro varia de acordo ao domínio.</p>
         </div>
-        <div class="col-lg-4">
-            <img class="rounded-circle" src="IMG/Icones/ex.png" alt="imagem do brasão (lado da medalha) para representar
-             o exército brasileiro" width="200" height="200" style="border: 5px solid #393841;">
-            <h2 id="t3">Exército</h2>
-            <p id="b3">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-        </div>
+    </div>
     </div>
 </div><br>
 <div class="container" style="padding-left: 50px;">
@@ -135,15 +128,18 @@ Nós da equipe Close separamos algumas informações sobre instituições que po
             <img class="rounded-circle" src="IMG/Icones/ong.png" alt="icone ilustrativo de uma mão segurando um megafone para
             representar ONGs" width="200" height="200" style="border: 5px solid #393841;">
             <h2 id="t4">ONGs</h2>
-            <p id="b4">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <p id="b4" style="text-align: justify;">Em relação á doação de  roupas para as ONGS, o ideal é verificar com a instituição, pelo telefone ou e-mail, quais as necessidades atuais de doação. Para entregar os objetos normalmente não é necessário marcar hora. Geralmente
+                 as instituições não buscam doações na casa dos doadores, sendo necessário entregar direto na instituição. Um exemplo de ONG atuante no Brasil é a AACD, que recebe doação de roupas, faz a triagem de todas as peças recebidas e encaminham 
+                 para cianças e adolescentes portadores de deficiências, de acordo com tamanho e demandas identificadas.</p>
         </div>
         <div class="col-lg-4">
-            <img class="rounded-circle" src="IMG/Icones/mny.png" alt="icone ilustrativo de uma mão segurando um saco de dinheiro 
-            para representar as trocas comerciais" width="200" height="200" style="border: 5px solid #393841;">
-            <h2 id="t5">Trocas Comerciais</h2>
-            <p id="b5">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <img class="rounded-circle" src="IMG/Icones/rlg.png" alt="icone minimalista com simbolos que representam 
+            diversos tipos fé diferentes para representar religião" width="200" height="200" 
+            style="border: 5px solid #393841;">
+            <h2 id="t2">InstituiçõesReligiosas</h2>
+            <p id="b2" style="text-align: justify;">As doações no Brasil voltadas à instituições religiosas consistem, em sua maioria, na doação de roupas, onde as peças são levadas para a igreja e então direcionadas a famílias carentes e de baixa renda. Um bom exemplo de tais ações, é a entidade cristã " Exército da Salvação", fundada na Europa mas atualmente está presente no mundo todo, onde seu principal objetivo é arrecadar doações, em sua maioria roupas, com intuito de encaminhar as mesmas à pessoas necessitadas.
+            Outro exemplo de doação recebido por essas instituições religiosas é o dízimo, que consiste na doação de dinheiro à essas igrejas. De acordo com pesquisas, no ano de 2019 às igrejas tanto católicas quanto evangélicas, movimentaram cerca de R$21,5 bilhões de reais.</p>
         </div>
-    </div>
 </div></center><br>
 <footer>
     <nav class="navbar navbar-default" role="navigation" id="rodp">
